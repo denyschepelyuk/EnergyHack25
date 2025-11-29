d9bafda5542171659168fa7dd2a80b6b4b8cd6b5
chepelid <denyschepeliuk@gmail.com>
2025-11-29 11:51:00 +0100

    .

--- src/GalacticBuf serialization/test_solution.py
+++ src/GalacticBuf serialization/test_solution.py
@@ -1,7 +1,8 @@
 from solution import GBObject, GBValue, serialize_message, parse_cli_args_to_object
 
 # Simulate CLI args: user_id=1001, name=Alice, scores=[100,200,300]
-args = ["user_id=1001", "name=Alice", "scores=[100,200,300]"]
+# args = ["user_id=1001", "name=Alice", "scores=[100,200,300]"]
+args = ["timestamp=1698765432", "trades=[{id:1, price:100}, {id:2, price:200}]"]
 
 obj = parse_cli_args_to_object(args)
 data = serialize_message(obj)
